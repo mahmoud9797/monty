2a72852695900421fe6559e9b79234c28c1b9746
Mahmoud <elmahallawy6@gmail.com>
2024-01-21 15:48:33 -0800

    mahmoud

--- pint.c
+++ pint.c
@@ -1,10 +1,15 @@
 #include "monty.h"
+/**
+ *pint - function to print the top element of the stack
+ *@stack: point to the top element
+ *@line_number: the number of line in file
+ */
 void pint(stack_t **stack, unsigned int line_number)
 {
-    if (!*stack)
-    {
-        printf("L%d: can't pint, stack empty\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-    printf("%d\n", (*stack)->n);
+	if (!*stack)
+	{
+		printf("L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
 }
