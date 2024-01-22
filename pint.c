69e29bdc59e5961ed2705a1946b51e0c2b6bf360
Mahmoud <elmahallawy6@gmail.com>
2024-01-22 06:55:36 -0800

    mahmoud

--- pint.c
+++ pint.c
@@ -8,7 +8,7 @@ void pint(stack_t **stack, unsigned int line_number)
 {
 	if (!*stack)
 	{
-		printf("L%d: can't pint, stack empty\n", line_number);
+		fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*stack)->n);
